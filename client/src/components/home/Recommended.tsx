d107b567e2a7bfd4a582d58f15d8d9ef3965fd53
abhayHeliverse <siemonab@gmail.com>
2024-02-16 18:39:15 +0530

    removed cached data

--- client/src/components/home/Recommended.tsx
+++ client/src/components/home/Recommended.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import SkeletonLoader from '../reusable/SkeletonLoader';
 import PaginationComponent from '../reusable/Pagination';
 import NothingToShow from '../reusable/NothingToShow';
-import {VideoType} from '../../types/types'
+import { VideoType } from '../../types/types'
 
 //Defining the type of props that he can accept
 interface propType {
@@ -32,11 +32,11 @@ function Recommended({ searchInput, searchQuery }: propType) {
                 await axios.get(
                     `/dashboard/recommended?search=${searchQuery}&pageNo=${pageNo}`,
                     {
-                        headers:{
+                        headers: {
                             Authorization: `Bearer ${token}`
                         }
                     }
-                    )
+                )
                     .then((response) => {
                         setAllVideos(response.data?.results)
                         setCount(response.data?.total_pages)
@@ -57,28 +57,31 @@ function Recommended({ searchInput, searchQuery }: propType) {
             </h1>
             {!loading ?
 
-                allVideos?.length > 0 ?
-                    <>
-                        <div className='grid bdsm:grid-cols-2 md:grid-cols-3 bdmd:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4'>
-                            {allVideos && allVideos.map((video) => {
-                                return (
-                                    <VideoCard
-                                        imageUrl={video?.poster_path}
-                                        title={video?.title || video?.name}
-                                        adult={video?.adult}
-                                        id={video?.id}
-                                        videoType={
-                                            video?.first_air_date ? "tv" : "movie"
-                                        }
-                                        releaseDate={video?.release_date || video?.first_air_date}
-                                    />
-                                )
-                            })}
-                        </div>
-                        <PaginationComponent count={count} setPageNo={setPageNo} />
-                    </>
-                    :
-                    <NothingToShow/>
+                <>
+                    {allVideos?.length > 0 ?
+                        <>
+                            <div className='grid bdsm:grid-cols-2 md:grid-cols-3 bdmd:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 mt-4'>
+                                {allVideos && allVideos.map((video) => {
+                                    return (
+                                        <VideoCard
+                                            imageUrl={video?.poster_path}
+                                            title={video?.title || video?.name}
+                                            adult={video?.adult}
+                                            id={video?.id}
+                                            videoType={
+                                                video?.first_air_date ? "tv" : "movie"
+                                            }
+                                            releaseDate={video?.release_date || video?.first_air_date}
+                                        />
+                                    )
+                                })}
+                            </div>
+                        </>
+                        :
+                        <NothingToShow />
+                    }
+                    <PaginationComponent count={count} setPageNo={setPageNo} />
+                </>
 
                 :
                 <SkeletonLoader />
